f27468cf21adb7b2ae03fb28c88d1af247a436d6
shifei520 <531187867@qq.com>
2021-12-13 22:50:45 +0800

    项目初始化

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,12 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    username: 'shifei'
+  },
+  getters: {
+    newName(state) {
+      return state.username + 'hello'
+    }
   },
   mutations: {
   },
